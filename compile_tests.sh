9c9db6d742d28158aa91da4531b327feb47ff8b4
Topi Talvitie <topi.talvitie@iki.fi>
2014-02-13 22:25:54 +0200

    Added some explicit type declarations to deal with older compilers.

--- compile_tests.sh
+++ compile_tests.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-g++ randomtest.cpp -o randomtest -O2 -Wall -g -std=c++11
-g++ smalltest.cpp -o smalltest -O2 -Wall -g -std=c++11
+g++ randomtest.cpp -o randomtest -O2 -Wall -g -std=c++0x
+g++ smalltest.cpp -o smalltest -O2 -Wall -g -std=c++0x
