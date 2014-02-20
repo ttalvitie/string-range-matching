940fe433115c37ebe73c3a890a651d9d6dbabe7f
Topi Talvitie <topi.talvitie@iki.fi>
2014-02-20 23:43:16 +0200

    Added forgotten linker flag.

--- compile_all.sh
+++ compile_all.sh
@@ -2,4 +2,4 @@
 
 g++ randomtest.cpp -o randomtest -O2 -Wall -g -std=c++0x
 g++ smalltest.cpp -o smalltest -O2 -Wall -g -std=c++0x
-g++ benchmark.cpp -o benchmark -DNDEBUG -O2 -Wall -g -std=c++0x
+g++ benchmark.cpp -o benchmark -DNDEBUG -O2 -Wall -g -std=c++0x -lrt
